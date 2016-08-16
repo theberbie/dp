8bfa6d31bbd13fed82b1f8f988e73cf080eafe50
Sara Ahbabou <ahbabousara@gmail.com>
2016-08-15 21:06:52 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Districtpawsee::Application.config.secret_key_base = '72e99c09afa6ef58f8371e2c01320aef73123aaa39de740a4aa45bdda4930d00a18f55a813a1fb9c6c13b4e06a5e14cc0216abbca9c2f42a0e9a5fab44072bb4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Districtpawsee::Application.config.session_store :cookie_store, key: '_districtpawsee_session'
